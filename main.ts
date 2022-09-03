a1c2dad9efd3dc594936d3c86576f1daec2bf95f
Jonny Smith <jdasmith@gmail.com>
2022-09-03 17:20:27 +0100

    identify with device name, serial number

--- main.ts
+++ main.ts
@@ -5,5 +5,9 @@ bluetooth.startTemperatureService()
 let num_connects = 0
 basic.forever(function () {
     basic.showNumber(num_connects)
-    basic.pause(5000)
+    basic.pause(2000)
+    basic.showString(control.deviceName())
+    basic.pause(2000)
+    basic.showString("" + (control.deviceSerialNumber()))
+    basic.pause(2000)
 })
