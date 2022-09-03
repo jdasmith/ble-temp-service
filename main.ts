134ccf7cedd1a0df7ea66e748476734aae2e06fa
Jonny Smith <jdasmith@gmail.com>
2022-09-03 17:14:21 +0100

    add delay and provide num connects as feedback

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
+bluetooth.onBluetoothConnected(function () {
+    num_connects += 1
+})
 bluetooth.startTemperatureService()
+let num_connects = 0
 basic.forever(function () {
-	
+    basic.showNumber(num_connects)
+    basic.pause(5000)
 })
