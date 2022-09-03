4266d7f340117becfc21fe44dab8d3f94b0abd1d
Jonny Smith <jdasmith@gmail.com>
2022-09-03 17:34:57 +0100

    Add uart bluetooth service

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ bluetooth.onBluetoothConnected(function () {
 })
 bluetooth.startTemperatureService()
 let num_connects = 0
+bluetooth.startUartService()
 basic.forever(function () {
     basic.showNumber(num_connects)
     basic.pause(2000)
@@ -10,4 +11,5 @@ basic.forever(function () {
     basic.pause(2000)
     basic.showString("" + (control.deviceSerialNumber()))
     basic.pause(2000)
+    bluetooth.uartWriteLine("T=" + convertToText(input.temperature()))
 })
