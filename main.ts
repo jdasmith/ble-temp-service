30e4251c3912db1a27362daf223f6b62fae9686a
Jonny Smith <jdasmith@gmail.com>
2022-11-06 14:09:15 +0000

    simple temp sensor

--- main.ts
+++ main.ts
@@ -1,15 +1,10 @@
 bluetooth.onBluetoothConnected(function () {
     num_connects += 1
+    basic.showIcon(IconNames.Happy)
 })
 bluetooth.startTemperatureService()
 let num_connects = 0
-bluetooth.startUartService()
 basic.forever(function () {
     basic.showNumber(num_connects)
     basic.pause(2000)
-    basic.showString(control.deviceName())
-    basic.pause(2000)
-    basic.showString("" + (control.deviceSerialNumber()))
-    basic.pause(2000)
-    bluetooth.uartWriteLine("T=" + convertToText(input.temperature()))
 })
