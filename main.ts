1c7fb6a46f3e884a616bdb65bf5161c27d54761f
Jonny Smith <jdasmith@gmail.com>
2023-03-24 20:32:21 +0000

    No forever loop, in case that is causing issues with the service

--- main.ts
+++ main.ts
@@ -2,13 +2,12 @@ bluetooth.onBluetoothConnected(function () {
     num_connects += 1
     basic.showIcon(IconNames.Happy)
 })
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
 bluetooth.startTemperatureService()
 let num_connects = 0
-basic.forever(function () {
-    basic.showString("Num Connects: ")
-    basic.showNumber(num_connects)
-    basic.pause(1000)
-    basic.showString("Temperature: ")
-    basic.showNumber(input.temperature())
-    basic.pause(1000)
-})
+bluetooth.startLEDService()
+bluetooth.startAccelerometerService()
+bluetooth.setTransmitPower(7)
+basic.showIcon(IconNames.Heart)
